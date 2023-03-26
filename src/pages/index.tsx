45a84c30f2a4e4e8e9049cb97a31f7bd68160431
Shubham Singh <shubhsingh01@gmail.com>
2023-03-26 13:27:09 +0530

    Font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,7 +36,7 @@ const PostView = (props: PostWithUser) => {
       <div className="flex text-slate-400 gap-1">
         <span>{`@${author.username}`}</span><span>1{` · ${dayjs(post.createdAt).fromNow()}`}</span>
       </div>
-      <span>{post.content}</span>
+      <span className="text-2xl">{post.content}</span>
     </div>
   </div>
 }
